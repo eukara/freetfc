de3ced89cf6acfccc298e01c5e3c0b1f2c224722
Xylemon <xylemon@posteo.net>
2022-07-14 21:07:46 -0700

    Wrench now has cells, and will display it in the HUD
    Some code cleanup

---
+++ src/client/defs.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2016-2022 Marco Cawthorne <marco@icculus.org>
+ * Copyright (c) 2022 Gethyn ThomasQuail <xylemon@posteo.net>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF MIND, USE, DATA OR PROFITS, WHETHER
+ * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#define MAX_A_CELLS 200
+
+var string g_dmsg_spr;
+var string g_tfchud1_spr;
+var string g_tfchud2_spr;
+var string g_tfchud3_spr;
+var string g_tfchud4_spr;
+var string g_tfchud5_spr;
+var string g_tfchud6_spr;
+var string g_tfchud7_spr;
